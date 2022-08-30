3e9e8dd71bb8d8404ebc6c6452acca9cedb2e3ae
GilbertoSantanaCosta <you@example.comg.s.d.c@hotmail.comgit config --global user.name Your>
2022-08-30 07:27:59 -0300

    Demarcando quantidade de minas vizinhas

--- src/br/com/projeto/cm/modelo/Campo.java
+++ src/br/com/projeto/cm/modelo/Campo.java
@@ -7,120 +7,108 @@ public class Campo {
 
 	private final int linha;
 	private final int coluna;
-	
+
 	private boolean aberto;
 	private boolean marcado;
 	private boolean minado;
-	
+
 	private List<Campo> vizinhos = new ArrayList<Campo>();
 	private List<CampoObservador> observadores = new ArrayList<CampoObservador>();
 
-	
 	public Campo(int linha, int coluna) {
-		
+
 		this.linha = linha;
 		this.coluna = coluna;
-		
+
 	}
-	
-	
+
 	public void registrarObservador(CampoObservador observador) {
 		observadores.add(observador);
 	}
-	
-	
+
 	private void notificarEvento(CampoEvento evento) {
-		
-		observadores.stream()
-					.forEach(o -> o.eventoOcorreu(this, evento));
+
+		observadores.stream().forEach(o -> o.eventoOcorreu(this, evento));
 	}
-	
-	
-	
-	
+
 	boolean adicionarVizinho(Campo vizinho) {
-		
+
 		boolean linhaDiferente = linha != vizinho.linha;
 		boolean colunaDiferente = coluna != vizinho.coluna;
 		boolean diagonal = linhaDiferente && colunaDiferente;
-		
+
 		int deltaLinha = Math.abs(linha - vizinho.linha);
 		int deltaColuna = Math.abs(coluna - vizinho.coluna);
 		int deltaGeral = deltaColuna + deltaLinha;
-		
-		if(deltaGeral == 1 && !diagonal) {
+
+		if (deltaGeral == 1 && !diagonal) {
 			vizinhos.add(vizinho);
 			return true;
-		}else if(deltaGeral == 2 && diagonal) {
+		} else if (deltaGeral == 2 && diagonal) {
 			vizinhos.add(vizinho);
 			return true;
 		} else {
 			return false;
 		}
-			
+
 	}
-	
-	
-	void alternarMarcacao() {
-		
-		if(!aberto) {
+
+	public void alternarMarcacao() {
+
+		if (!aberto) {
 			marcado = !marcado;
-			
-			if(marcado) {
+
+			if (marcado) {
 				notificarEvento(CampoEvento.MARCAR);
-			}else {
+			} else {
 				notificarEvento(CampoEvento.DESMARCAR);
 			}
 		}
 	}
-	
-	boolean abrir() {
-		
-		if(!aberto && !marcado) {
-			
-			
-			
-			if(minado) {
+
+	public boolean abrir() {
+
+		if (!aberto && !marcado) {
+
+			if (minado) {
 				notificarEvento(CampoEvento.EXPLODIR);
 				return true;
 			}
-			
-			setAberto(aberto);
-			
-			if(vizinhancaSegura()){
-				
+
+			setAberto(true);
+
+			if (vizinhancaSegura()) {
+
 				vizinhos.forEach(v -> v.abrir());
 			}
-			
+
 			return true;
-		}else {
+		} else {
 			return false;
-		}	
+		}
 	}
-	
-	
-	boolean vizinhancaSegura() {
+
+	public boolean vizinhancaSegura() {
 		return vizinhos.stream().noneMatch(v -> v.minado);
 	}
-	
+
 	void minar() {
 		minado = true;
 	}
-	
+
 	public boolean isMarcado() {
 		return marcado;
 	}
 
 	public boolean isAberto() {
-		
+
 		return aberto;
 	}
-	
-	
+
 	void setAberto(boolean aberto) {
 		this.aberto = aberto;
-		
-		if(aberto) {
+
+		if (aberto) {
 			notificarEvento(CampoEvento.ABRIR);
 		}
 	}
@@ -130,7 +118,7 @@ public class Campo {
 	}
 
 	public boolean isFechado() {
-		
+
 		return !isAberto();
 	}
 
@@ -141,41 +129,39 @@ public class Campo {
 	public int getColuna() {
 		return coluna;
 	}
-	
+
 	public boolean objetivoAlcancado() {
 		boolean desvendado = aberto && !minado;
 		boolean protegido = minado && marcado;
-		
+
 		return desvendado || protegido;
 	}
-	
-	long minasNaVizinhanca() {
-	return vizinhos.stream().filter(v -> v.minado).count();
+
+	public int minasNaVizinhanca() {
+		return (int) vizinhos.stream().filter(v -> v.minado).count();
 	}
-	
+
 	void reiniciar() {
 		aberto = false;
 		minado = false;
 		marcado = false;
 	}
-	
-		
+
 	public String toString() {
-		
-		if(marcado) {
+
+		if (marcado) {
 			return "X";
-		} else if(aberto && minado) {
+		} else if (aberto && minado) {
 			return "*";
-		}else if(aberto && minasNaVizinhanca() > 0) {
+		} else if (aberto && minasNaVizinhanca() > 0) {
 			return Long.toString(minasNaVizinhanca());
-		}if(aberto) {
+		}
+		if (aberto) {
 			return " ";
 		} else {
 			return "?";
 		}
-		
+
 	}
 
-	
-	
 }
--- src/br/com/projeto/cm/visao/BotaoCampo.java
+++ src/br/com/projeto/cm/visao/BotaoCampo.java
@@ -1,14 +1,119 @@
 package br.com.projeto.cm.visao;
 
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import java.awt.event.MouseListener;
 
 import br.com.projeto.cm.modelo.Campo;
+import br.com.projeto.cm.modelo.CampoEvento;
+import br.com.projeto.cm.modelo.CampoObservador;
 
 @SuppressWarnings("serial")
-public class BotaoCampo extends JButton {
+public class BotaoCampo extends JButton implements CampoObservador , MouseListener {
+	
+	private final Color BG_PADRAO = new Color(184,184,184);
+	private final Color BG_MARCAR = new Color(8,179,247);
+	private final Color BG_EXPLODIR = new Color(189,66,68);
+	private final Color BG_TEXTOVERDE = new Color(0,100,0);
+	
+	
+	
+	private Campo campo;
 
 	public BotaoCampo(Campo campo) {
+		this.campo = campo;
+		setBackground(BG_PADRAO);
+		campo.registrarObservador(this);
+		
+		addMouseListener(this);
 	}
+
+	@Override
+	public void eventoOcorreu(Campo campo, CampoEvento evento) {
+		switch(evento) {
+		case ABRIR:
+			aplicarEstiloAbrir();
+			break;
+		case MARCAR:
+			aplicarEstiloMarcar();
+			break;
+		case EXPLODIR:
+			aplicarEstiloExplodir();
+			break;
+		default: 
+			aplicarEstiloPadrao();
+			
+		}
+		
+	}
+
+	private void aplicarEstiloExplodir() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void aplicarEstiloMarcar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void aplicarEstiloPadrao() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void aplicarEstiloAbrir() {
+		setBackground(BG_PADRAO);
+		setBorder(BorderFactory.createLineBorder(Color.GRAY));
+		
+		switch (campo.minasNaVizinhanca()) {
+		case 1:
+			setForeground(BG_TEXTOVERDE);
+			break;
+		case 2:
+			setForeground(Color.BLUE);
+			break;
+		case 3:
+			setForeground(Color.YELLOW);
+			break;
+		case 4:
+		case 5:
+		case 6:
+			setForeground(Color.RED);
+			break;
+		default:
+			setForeground(Color.PINK);
+			
+		}
+		
+		String valor = !campo.vizinhancaSegura() 
+						? campo.minasNaVizinhanca() + "" : "";
+		setText(valor);
+	}
+	
+	
+
+	
+	
+	
+	@Override
+	public void mousePressed(MouseEvent e){
+		if (e.getButton() == 1) {
+			campo.abrir();
+		}else {
+			campo.alternarMarcacao();
+		}
+	}
+	
+	public void mouseClicked(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e){}
+	public void mouseEntered(MouseEvent e){}
+	public void mouseExited(MouseEvent e){}
+	public void mouseDragged(MouseEvent e){}
+	public void mouseMoved(MouseEvent e){}
 
 	
 }
